7c9a2b16249fc301a7b45ca89d0dc2ef31a6a3c1
strOut- <chrisdaly1988@gmail.com>
2014-08-05 01:01:55 +0100

    8th commit
    added my own page rank

---
+++ Page Rank/Google Page Rank.py
@@ -0,0 +1,60 @@
+import httplib
+
+
+# define the host
+prhost='toolbarqueries.google.com' 
+
+# define the path query in which to substitute the specific query parameters later
+prpath='/tbr?client=navclient-auto&ch=%s&features=Rank&q=info:%s'
+
+
+# Returns the result of Google's hashing algorithm on a URL
+###############################
+def getHash(query):
+
+	# this seed is google's salt for generating the hash
+	SEED = "Mining PageRank is AGAINST GOOGLE'S TERMS OF SERVICE."      
+
+	# dgoogle's initialized hex value
+	Result = 0x01020345
+ 
+ 	# google's hashing algorithm, that operates bitwise and returns a hex value
+	for i in range(len(query)):
+		Result ^= ord(SEED[i%len(SEED)]) ^ ord(query[i])
+		Result = Result >> 23 | Result << 9
+		Result &= 0xffffffff
+
+	return '8%x' % Result
+
+
+# Queries google for the PageRank
+###############################
+def getPageRank(query):
+
+	# create a connection with the host
+	conn = httplib.HTTPConnection(prhost)   
+
+	# generate google's hash for the URL
+	hash = getHash(query) 
+
+	# substitute in the URL and its hash into the path
+	path = prpath % (hash,query)   
+
+	# request the information from google
+	conn.request("GET", path)  
+
+	# get and parse the response
+	response = conn.getresponse()  
+	data = response.read()  
+
+	# close the connection
+	conn.close() 
+
+	return data.split(":")[-1]     
+
+# Prints out the PageRank
+def PrintPageRank(query):
+	print query, " PageRank ", getPageRank(query)
+	return
+
+PrintPageRank("http://www.reddit.com")
---
+++ Page Rank/My Page Rank.py
@@ -0,0 +1,147 @@
+# http://glowingpython.blogspot.com/2011/05/four-ways-to-compute-google-pagerank.html
+from numpy import *
+
+
+# Basic power method. Stops after the number of iteration as specified in the "iter" paramneter
+###############################
+def powerMethodBase(A, x0, iter):
+
+ for i in range(iter):
+  x0 = dot(A,x0)
+  x0 = x0 / linalg.norm(x0,1)
+
+  return x0
+
+
+# Basic power method. Stops after the number of iteration as specified in the "iter" paramneter
+# or after the improvement is less than threshold (thresh parameter)
+###############################
+def powerMethodBase1(A, x0, thresh,iter):
+
+  x0 = x0 / linalg.norm(x0, 1)
+  x1 = x0
+
+  for i in range(iter):
+   x0 = dot(A,x0)
+   x0 = x0 / linalg.norm(x0, 1)
+   stop = linalg.norm(x0 - x1, 1)
+
+   if stop < thresh:
+    print "Method 5 Stop ", stop, " After ", i, " iterations compare to default", iter 
+    break
+
+   x1 = x0
+
+  return x0
+ 
+
+# Power method to compute the maximal real eigenvector of the matrix M built on top
+# of the input matrix A 
+###############################
+def powerMethod(A,x0,m,iter):
+
+ n = A.shape[1]
+ delta = m * (array([1] * n, dtype='float64') / n) 
+
+ for i in range(iter):
+  x0 = dot((1 - m), dot(A, x0)) + delta
+
+ return x0
+
+
+# Uses the eig function to compute eigenvectors
+###############################
+def maximalEigenvector(A):
+
+ n = A.shape[1]
+ w,v = linalg.eig(A)
+
+ return abs(real(v[:n, 0])/linalg.norm(v[:n, 0], 1))
+
+
+# Solves linear equations of the system (I-(1-m)*A)*x = m*s
+###############################
+def linearEquations(A,m):
+
+ n = A.shape[1]
+ C = eye(n, n) - dot((1 - m),A)
+ b = m*(array([1] * n, dtype='float64') / n)
+
+ return linalg.solve(C,b)
+
+# Returns the matrix M of the web described by A
+###############################
+def getTeleMatrix(A, m):
+
+ n = A.shape[1]
+ S = ones((n, n)) / n
+
+ return (1 - m) * A + m*S
+
+
+# Example
+###############################
+A = array([ [0,     0,     0,     1, 0, 1],
+            [1/2.0, 0,     0,     0, 0, 0],
+            [0,     1/2.0, 0,     0, 0, 0],
+            [0,     1/2.0, 1/3.0, 0, 0, 0],
+            [0,     0,     1/3.0, 0, 0, 0],
+            [1/2.0, 0,     1/3.0, 0, 1, 0 ] ])
+
+
+# get the number of rows
+n = A.shape[1]
+
+# damping factor of 0.15(indicates the probabilty of the user not chosing 
+# any of the links on the page he is on.)
+m = 0.15        
+
+M = getTeleMatrix(A,m)
+
+x0 = ones(n)/ n
+x1 = powerMethod(A,x0,m,130)
+x2 = powerMethodBase(M,x0,130)
+x5 = powerMethodBase1(M,x0,1.0e-6, 130)
+x3 = maximalEigenvector(M)
+if m > 0.001:
+	x4 = linearEquations(A,m)
+
+# comparison of the five methods
+print "\nDamping factor ", m, " Matrix Size ", n
+print "1 ", x1
+print "2 ", x2
+print "3 ", x3
+if m > 0.001:
+	print "4 ", x4
+print "5 ", x5
+
+obs = array([ 
+	[0,     0, 0, 0, 0, 0, 1/3.0, 0],
+	[1/2.0, 0, 1/2.0, 1/3.0, 0, 0, 0, 0],
+	[1/2.0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 0, 0, 0, 0, 0, 0],
+	[0, 0, 1/2.0, 1/3.0, 0, 0, 1/3.0, 0],
+	[0, 0, 0, 1/3.0, 1/3.0, 0, 0, 1/2.0],
+	[0, 0, 0, 0, 1/3.0, 0, 0, 1/2.0],		
+	[0, 0, 0, 0, 1/3.0, 1, 1/3.0, 0] ])
+            
+n = obs.shape[1] # obs is n x n
+m = 0.0	# No damping
+M = getTeleMatrix(obs,m)
+
+x0 = ones(n)/ n
+x1 = powerMethod(obs,x0,m,130)
+x2 = powerMethodBase(M,x0,130)
+x5 = powerMethodBase1(M,x0,1.0e-7, 130)
+x3 = maximalEigenvector(M)
+if m > 0.001:
+	x4 = linearEquations(H,m)
+
+# comparison of the five methods
+print "\nDamping factor ", m, " Matrix Size ", n
+print "1 ", x1
+print "2 ", x2
+print "3 ", x3
+if m > 0.001:
+	print "4 ", x4
+print "5 ", x5
